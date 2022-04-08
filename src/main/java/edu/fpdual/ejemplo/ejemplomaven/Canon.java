5f14c11b53897820f3ea244b3e3d5a450bee35af
i.barboteo.company <irenebarboteo@gmail.com>
2022-04-08 13:53:15 +0200

    Clase 08/04

---
+++ src/main/java/edu/fpdual/ejemplo/ejemplomaven/Canon.java
@@ -0,0 +1,14 @@
+package edu.fpdual.ejemplo.ejemplomaven;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Canon {
+    private String letra;
+    private int frecuencia;
+
+}
